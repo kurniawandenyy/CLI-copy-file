e79224d576e997f669c3332b64c054d91cb76006
deny <denykurniawan146@gmail.com>
2021-12-29 22:33:11 +0700

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
 # CLI-copy-file
 A simple tools to copy file log and convert to txt or json extension via CLI
+
+
+# Simple API for Movies App 
+
+## Table of contents
+* [General info](#general-info)
+* [Built With](#built-with)
+* [Requirements](#requirements)
+* [Setup](#setup)
+* [Contact](#contact)
+* [Contributors](#contributors)
+
+## General info
+A simple tools to copy file log and convert to txt or json extension via CLI. Built with NodeJs. [More about NodeJS](https://nodejs.org/en/)
+	
+## Built With
+Project is created with:
+
+[![Node.js](https://img.shields.io/badge/Node.js-v.12.21-green.svg?style=rounded-square)](https://nodejs.org/)
+
+## Requirements
+1. <a href="https://nodejs.org/en/">NodeJs</a>
+
+## Setup
+Steps to run this project:
+
+ - Clone this project ```git clone https://github.com/kurniawandenyy/CLI-copy-file.git```
+ - Open terminal and go to your apps directory
+ - Run ```npm install```
+ - Then run ```npm link``` to link app
+ - And then run ```copy-file <file> -t <fileType> (text/json) -o <output location>``` e.g., ```copy-file /var/log/error.log -t text -o /home/xlog.txt```
+
+## Contact
+
+If you want to contact me you can reach me at <denykurniawan146@gmail.com>.
+
+## Contributors
+
+<center>
+  <table>
+    <tr>
+      <td align="center">
+        <a href="https://github.com/kurniawandenyy">
+          <img width="100" src="https://avatars1.githubusercontent.com/u/48039021?s=460&v=4" alt="Deny Kurniawan"><br/>
+          <sub><b>Deny Kurniawan</b></sub>
+        </a>
+      </td>
+    </tr>
+  </table>
+</center>
---
+++ index.js
@@ -0,0 +1,51 @@
+#! /usr/bin/env node
+import { Command, Option } from 'commander'
+import fs from 'fs'
+import path from 'path'
+
+const program = new Command()
+
+//declare default extension
+let ext = 'txt'
+
+program
+    .version('1.0.0')
+    .description('Simple tools to copy file via CLI')
+
+program
+    .argument('<file>')
+    .addOption(new Option('-t, --type <type>', 'Copy file to specific extension').default('text').choices(['text', 'json']))
+    .option('-o, --output <output>', 'Copy file to specific location')
+    .action((file, options) => {
+
+        let output = path.dirname(file) + '/'
+        //check file extension
+        if (options.type == 'text') ext = 'txt'
+        if (options.type == 'json') ext = 'json'
+
+        //validate output location
+        if (options.output) {
+            let arr = options.output.split('.')
+            if (arr[arr.length - 1] != ext) {
+                return console.log('Invalid file extension')
+            } else {
+                //remove extension
+                output = path.parse(options.output).dir + path.parse(options.output).name
+            }
+        } else {
+            output += path.parse(path.basename(file, '.*')).name
+        }
+
+        try {
+            fs.copyFile(file, output + '.' + ext, err => {
+                if (err) return console.log(err)
+
+                console.log(`copied success (${output.replace(/\\/g, '/')}.${ext})`)
+            })
+        } catch (error) {
+            console.error(error)
+        }
+    })
+
+program.showHelpAfterError('(add --help for additional information)')
+program.parse(process.argv)
